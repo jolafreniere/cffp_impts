e9b28964bc2046ae857553c2ec5213e3d0cb9338
Jonathan Lafrenière <46546590+jolafreniere@users.noreply.github.com>
2021-02-07 16:14:18 -0500

    Create parameters.js

---
+++ parameters.js
@@ -0,0 +1,185 @@
+let CREDIT_PARAMETERS ={
+    "AGE_RET_PVS": {
+        "LINE0_MAXREV": 51741,
+        "LINE1_MAXREV": 68871,
+        "LINE2_MAXREV": 66957,
+        "LINE3_MAXREV": 84087,
+        "LINE4_MAXREV": 61074,
+        "LINE5_MAXREV": 72594,
+        "LINE6_MAXREV": 76290,
+        "LINE7_MAXREV": 87810,
+        "LINE8_MAXREV": 49826,
+        "LINE9_MAXREV": 59159,
+        "LINE10_MAXREV": 70679,
+        "LINE11_MAXREV": 43943,
+        "LINE12_MAXREV": 55463
+    },
+    "PROV_PRIM_TRAVAIL": {
+        "NPNK_MINREV": 2400,
+        "NPNK_MAXREV": 19456,
+        "NPK_MINREV": 2400,
+        "NPK_MAXREV": 35680,
+        "PNK_MINREV": 3600,
+        "PNK_MAXREV": 30217,
+        "PK_MINREV": 3600,
+        "PK_MAXREV": 49044
+    },
+    "FED_ALLOC_ENFANT": {
+        "_0BELOW6_1ABOVE6_MAXREV": 163069,
+        "_0BELOW6_2ABOVE6_MAXREV": 177999,
+        "_0BELOW6_3ABOVE6_MAXREV": 191274,
+        "_0BELOW6_4ABOVE6_MAXREV": 215400,
+        "_1BELOW6_0ABOVE6_MAXREV": 195475,
+        "_1BELOW6_1ABOVE6_MAXREV": 196192,
+        "_1BELOW6_2ABOVE6_MAXREV": 204236,
+        "_1BELOW6_3ABOVE6_MAXREV": 226316,
+        "_2BELOW6_0ABOVE6_MAXREV": 214385,
+        "_2BELOW6_1ABOVE6_MAXREV": 217199,
+        "_2BELOW6_2ABOVE6_MAXREV": 237232,
+        "_3BELOW6_0ABOVE6_MAXREV": 230161,
+        "_3BELOW6_1ABOVE6_MAXREV": 248148,
+        "_4BELOW6_0ABOVE6_MAXREV": 259064
+    },
+    "FED_TPS": {
+        "NP0_MAXREV": 46649,
+        "NP1_MAXREV": 52449,
+        "NP2_MAXREV": 55509,
+        "NP3_MAXREV": 58569,
+        "NP4_MAXREV": 61629,
+        "P0_MAXREV": 49389,
+        "P1_MAXREV": 52449,
+        "P2_MAXREV": 55509,
+        "P3_MAXREV": 58569,
+        "P4_MAXREV": 61629
+    },
+    "PROV_SOLIDARITE": {
+        "NP_L_0K_MAXREV": 51433,
+        "NP_L_1K_MAXREV": 53450,
+        "NP_L_2K_MAXREV": 55467,
+        "NP_L_3K_MAXREV": 57483,
+        "NP_L_4K_MAXREV": 59550,
+        "NP_2L_MAXREV": 45392,
+        "NP_3L_MAXREV": 43483,
+        "NP_4L_MAXREV": 42529,
+        "P_MAXREV": 54267,
+        "P_L_0K_MAXREV": 55983,
+        "P_L_1K_MAXREV": 58000,
+        "P_L_2K_MAXREV": 60017,
+        "P_L_3K_MAXREV": 62033,
+        "P_L_4K_MAXREV": 64050
+    },
+    "FED_ALLOC_TRAVAILLEURS": {
+        "NPNK_MINREV": 2400,
+        "NPNK_MAXREV": 23458,
+        "NPK_MINREV": 2400,
+        "NPK_MAXREV": 18305,
+        "PNK_MINREV": 3600,
+        "PNK_MAXREV": 36308,
+        "PK_MINREV": 3600,
+        "PK_MAXREV": 27629
+    },
+    "FED_AGE": {
+        "MIN_AGE": 65,
+        "MAX_REV": 87750
+    },
+    "PROV_ACT_AINES": {
+        "MIN_AGE": 70,
+        "MAX_REV_FAM": 42216,
+        "HAS_PAID_INSCR_FEES": true
+    },
+    "PROV_SOUTIEN_AINES": {
+        "MIN_AGE": 70,
+        "HAS_PARTNER": false,
+        "MAX_REV": 26945
+    },
+    "PROV_SOUTIEN_AINES_2": {
+        "MIN_AGE": 70,
+        "HAS_PARTNER": true,
+        "MAX_PARTNER_AGE": 70,
+        "MAX_REV_FAM": 41285
+    },
+    "PROV_SOUTIEN_AINES_3": {
+        "MIN_AGE": 70,
+        "HAS_PARTNER": true,
+        "MIN_PARTNER_AGE": 70,
+        "MAX_REV_FAM": 45345
+    },
+    "FED_ACCES_DOMICILIAIRE": {
+        "MIN_AGE": 65,
+        "HAS_PARTNER": true,
+        "MIN_PARTNER_AGE": 65,
+        "HAS_PAID_RENOV": true
+    },
+    "PROV_MAINTIEN_DOM_AINES": {
+        "MIN_AGE": 70,
+        "HAS_PARTNER": false,
+        "HAS_PARTNER_2": true,
+        "MAX_PARTNER_AGE": 70,
+        "MAX_REV_FAM": 285880,
+        "HAS_PAID_SOUTIEN_DOMICILE": true
+    },
+    "PROV_MAINTIEN_DOM_AINES_2": {
+        "MIN_AGE": 70,
+        "HAS_PARTNER": true,
+        "MIN_PARTNER_AGE": 70,
+        "MAX_REV_FAM": 513380,
+        "HAS_PAID_FEES": true
+    },
+    "PROV_FRAC_REV_RETRAITE": {
+        "MIN_AGE": 65,
+        "HAS_PARTNER": true,
+        "HAS_REV_RETRAITE": true
+    },
+    "PROV_ACTIVITE_ENFANT": {
+        "MAX_REV_FAM": 138525,
+        "HAS_PAID_FEES": true
+    },
+    "PROV_ALLOC_FAMILLE": {
+        "KID_AMOUNT": 1,
+        "AGE": 18
+    },
+    "FED_EPOUX_CONJOINT": {
+        "HAS_PARTNER": true,
+        "MAX_REV_PARTNER": 12069
+    },
+    "FED_PERSONNE_A_CHARGE": {
+        "HAS_PARTNER": false,
+        "KID_AMOUNT": 1,
+        "MAX_KID_AGE": 18,
+        "MAX_KID_REV": 12069
+    },
+    "PROV_PROL_CARRIERE": {
+        "MIN_AGE": 60,
+        "HAS_REV_EMPL": true,
+        "MIN_REV": 5000
+    },
+    "PROV_MONTANT_POSTSEC": {
+        "MIN_KID_AMOUNT": 1,
+        "MAX_AGE": 18,
+        "KID_IS_STUDYING_FULL_TIME": true,
+        "MAX_KID_REV": 5866
+    },
+    "PROV_MONTANT_POSTSEC_1": {
+        "MIN_KID_AMOUNT": 1,
+        "MIN_AGE": 18,
+        "KID_IS_STUDYING_FULL_TIME": true,
+        "MAX_KID_REV": 10482
+    },
+    "PROV_PERSONNE_CHARGE": {
+        "MIN_KID_AMOUNT": 1,
+        "MIN_AGE": 18,
+        "KID_IS_STUDYING_FULL_TIME": true,
+        "MAX_KID_REV": 4274
+    },
+    "FED_SUPPL_FRAIS_MED": {
+        "HAS_FRAIS_MEDIC": true,
+        "HAS_REV_EMPL": true,
+        "MAX_REV_FAM": 52599,
+        "MIN_REV_FAM": 52599
+    },
+    "PROV_FRAIS_MEDIC": {
+        "HAS_FRAIS_MEDIC": true,
+        "HAS_REV_EMPL": true,
+        "MAX_REV_FAM": 47400
+    }
+}
